ca7c614176aeb627dfed4d91dc0d94cce20830fa
Sveta <new-lana1@yandex.ru>
2017-11-03 00:12:18 +0300

    print message

---
+++ src/main/java/com/vsuet/uits/sveta/algo/rec/PrintMessage.java
@@ -0,0 +1,14 @@
+package com.vsuet.uits.sveta.algo.rec;
+
+public class PrintMessage{
+    private static void printNTimes(final int n, final String message){
+        if (n != 0) {
+            System.out.println(message);
+            printNTimes(n-1, message);
+        }
+    }
+    public static void main(final String[] args) {
+        printNTimes(5, "SOS");
+    }
+}
+
